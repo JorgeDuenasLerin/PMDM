e2fa061e5b0b3c6ef6cc3b08e99a4566784262b4
JorgeDuenasLerin <jorge.duenas.lerin@gmail.com>
2020-10-09 12:52:13 +0200

    Acticidad de pruebas

---
+++ app/src/main/java/org/duenaslerin/pmdm/Pruebas.java
@@ -0,0 +1,30 @@
+package org.duenaslerin.pmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+public class Pruebas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout layout = new LinearLayout(this);
+        Spinner spinner = new Spinner(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
+        adapter.add("Lunes");
+        adapter.add("Martes");
+        adapter.add("Miércoles");
+        adapter.add("Jueves");
+        adapter.add("Viernes");
+        adapter.add("Sábado");
+        adapter.add("Domingo");
+        spinner.setAdapter(adapter);
+        layout.addView(spinner);
+        setContentView(layout);
+        //setContentView(R.layout.activity_pruebas);
+    }
+}
