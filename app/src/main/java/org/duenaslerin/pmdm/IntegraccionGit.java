ffb59a2d64ec2007caae2217ab0719074ae65d1b
JorgeDuenasLerin <jorge.duenas.lerin@gmail.com>
2020-10-09 12:34:36 +0200

    Una app hola mundo!

---
+++ app/src/main/java/org/duenaslerin/pmdm/IntegraccionGit.java
@@ -0,0 +1,14 @@
+package org.duenaslerin.pmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntegraccionGit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_integraccion_git);
+    }
+}
