134bdc99dbffd02b873690a83c674508e5bbe42f
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2020-10-14 16:52:46 +0200

    Ejemplo de variables en clase

--- app/src/main/java/org/duenaslerin/pmdm/SegundaActividad.java
+++ app/src/main/java/org/duenaslerin/pmdm/SegundaActividad.java
@@ -3,12 +3,36 @@ package org.duenaslerin.pmdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SegundaActividad extends AppCompatActivity {
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_segunda_actividad);
+        //setContentView(R.layout.activity_segunda_actividad);
+        crearUI();
+
+        View.OnClickListener elQueManeja = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+
+        asociarEventos(elQueManeja);
+
+        setContentView(b);
+    }
+
+    private void asociarEventos(View.OnClickListener parametroManejador) {
+        b.setOnClickListener(parametroManejador);
+    }
+
+    private void crearUI(){
+        b = new Button(this);
     }
 }
