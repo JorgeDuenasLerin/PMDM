d933d4a758552fc940d54423c41650dbe35f3755
JorgeDuenasLerin <jorge.duenas.lerin@gmail.com>
2020-10-22 10:38:04 +0200

    Actualizado el WebView

--- app/src/main/java/org/duenaslerin/pmdm/U413_WebView.java
+++ app/src/main/java/org/duenaslerin/pmdm/U413_WebView.java
@@ -16,14 +16,13 @@ public class U413_WebView extends AppCompatActivity {
         final WebView wbView = new WebView(this);
 
         WebSettings settings = wbView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        //wbView.loadUrl("https://play.google.com/store/apps");
-        wbView.loadUrl("https://site.educa.madrid.org/ies.villablanca.madrid/index.php/departamento-de-informatica/");
-        wbView.clearView();
-        wbView.measure(100, 100);
-        settings.setUseWideViewPort(true);
-        settings.setLoadWithOverviewMode(true);
 
+        //settings.setJavaScriptEnabled(true);
+        //wbView.loadUrl("https://play.google.com/store/apps");
+
+        wbView.loadUrl("https://site.educa.madrid.org/ies.villablanca.madrid/index.php/departamento-de-informatica/");
+        // Usar local host
+        //wbView.loadUrl("http://10.0.2.2:8000/index.html");
 
         setContentView(wbView);
     }
