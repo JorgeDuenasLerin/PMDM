13c79637844d6d9a69c343798cfde7930908309e
JorgeDuenasLerin <jorge.duenas.lerin@gmail.com>
2020-10-23 17:32:35 +0200

    Layouts

---
+++ app/src/main/java/org/duenaslerin/pmdm/U500_LayoutsXML.java
@@ -0,0 +1,14 @@
+package org.duenaslerin.pmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class U500_LayoutsXML extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_u5__layouts_x_m_l);
+    }
+}
