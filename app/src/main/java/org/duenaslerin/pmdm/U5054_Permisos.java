ab7b34de4ab5faab3cc68da5363b2a536496c257
JorgeDuenasLerin <jorge.duenas.lerin@gmail.com>
2020-11-18 12:25:14 +0100

    Comienzo de permisos

---
+++ app/src/main/java/org/duenaslerin/pmdm/U5054_Permisos.java
@@ -0,0 +1,14 @@
+package org.duenaslerin.pmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class U5054_Permisos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_u5054__permisos);
+    }
+}
